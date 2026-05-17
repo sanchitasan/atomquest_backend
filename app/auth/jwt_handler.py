4e3d79178f0c2f3207f05401015bd825118bb885
Sanchita <sanchita@Sanchitas-MacBook-Air.local>
2026-05-17 15:33:12 +0530

    changes done

--- app/auth/jwt_handler.py
+++ app/auth/jwt_handler.py
@@ -1,8 +1,13 @@
+import os
+
 from jose import jwt
 from datetime import datetime, timedelta
+from dotenv import load_dotenv
 
-SECRET_KEY = "supersecretkey"
-ALGORITHM = "HS256"
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
 
 def create_access_token(data: dict):
 
